21160b348a6de263b23d50f10262daafbec53834
36118052019.1n <36118052019.1n@36DE194545.senacrjedu.local>
2019-05-29 20:08:20 -0300

    Atualizado na aula - 29/05/2019

--- javaOO/src/javaOO2/Conta.java
+++ javaOO/src/javaOO2/Conta.java
@@ -1,41 +1,52 @@
+//CRTL + A = Seleciona tudo
+//CTRL + I - Indentação correta
+
 package javaOO2;
 
 public class Conta {
 	//escopo de variáveis da classe: existe enquanto a linha de execução da classe for usada
-	
+
 	//atributos
 	int codigo;
 	String nome;
 	int numero;
 	double saldo; //float = precisão menor em relação ao double = 64bits, range maior
 	double limite;
-	
+
 	//métodos 
 	//valor é parâmetro para o método sacar, variável existe em outro lugar
 	//escopo variáveis do método: exemplo valor, saldoAlterado = criadas a partir de um método
-	
-	void sacar(double valor){
-		
+
+	boolean sacar(double valor){ //void = sem retorno; boolean = marcador booleano: retorna true ou false no m~étodo
+
 		//Opção 1: com variável auxiliar
 		//double saldoAlterado = saldo - valor;
 		//saldo = saldoAlterado;
-		
+
 		//Opção 2:
 		//saldo = saldo - valor;
-		
+
 		//Opção 3:
 		//this.saldo -= valor;
-		
-        if (valor > this.saldo){ 
-            System.out.println("Não posso sacar um valor maior que o saldo!");
-        } else {
-            this.saldo = this.saldo - valor;
-        }
+
+		// if (valor > this.saldo){ 
+		//     System.out.println("Não posso sacar um valor maior que o saldo!");
+		// } else {
+		//     this.saldo = this.saldo - valor;
+		// }
+
+		if((saldo+limite) < valor) { //variável azul pertence a esta classe; variável vermelha não pertence a esta classe
+			this.saldo -=valor;
+			return false;
+		} else {
+			this.saldo -= valor;
+			return true;
+		}
 	}
-	
+
 	void despositar(double valor) {
 		this.saldo += valor;
-		
+
 		//this. = diretiva de desambiguação = algumas vezes a boa prática é repetir na variável do método o nome da variável da classe.
 		//this.saldo = variável saldo pertencente a esta classe Conta
 	}
--- javaOO/src/javaOO2/UsaConta.java
+++ javaOO/src/javaOO2/UsaConta.java
@@ -2,57 +2,71 @@ package javaOO2;
 
 public class UsaConta {
 	public static void main(String[] args) { //classe principal, controladora;
-		
-	   //declaração  //instanciação
+
+		//declaração  //instanciação
 		Conta c1 = new Conta();  //cria em memória uma cópia da classe Conta na variável c1;
 		Conta c2 = new Conta();
-		
+
 		//especialização
 		c1.numero = 01;
 		c1.nome = "Robert";
 		c1.saldo = 1000.0;
 		c1.limite = 500.0;
-		
+
 		c2.numero = 02;
 		c2.nome = "Pedro";
 		c2.saldo = 5000.0;
 		c2.limite = 100.0;
-		
+
 		System.out.println("Número da conta: "+c1.numero);
 		System.out.println("Titular: "+c1.nome);
 		System.out.println("Saldo atual: "+c1.saldo);
 		System.out.println("Limite:  "+c1.limite);
-		
+
 		System.out.println("         ");
-		
+
 		System.out.println("Número da conta: "+c2.numero);
 		System.out.println("Titular: "+c2.nome);
 		System.out.println("Saldo atual: "+c2.saldo);
 		System.out.println("Limite:  "+c2.limite);
-		
+
 		System.out.println("         ");
 		System.out.println("SAQUE: ");
-		
+
 		//método sacar com parâmetro de valor = 100
+		
+		//Uma variável foi criada (ValorSaque) passando o valor do saque que será de 3500.
+		//Abaixo uma outra variável do tipo booleano atribuída ao método  c1.sacar(ValorSaque);
+		//No If baixo é criada as condições segundo os parâmetros de true/false do boolean;
+
+		double ValorSaque = 3500;
+		boolean resultado = c1.sacar(ValorSaque);
+
+		if(resultado) {
+			System.out.println("Saldo atualizado após sacar 100 reais do Robert: "+c1.saldo); //1ª parte true do boolean
+		} else {
+			System.out.println("Não posso sacar um valor maior que o saldo!"); //2ª parte false do boolean
+		}
+
 		c1.sacar(500);
 		System.out.println("Saldo atualizado após sacar 100 reais do Robert: "+c1.saldo);
-		
+
 		c2.sacar(200);
 		System.out.println("Saldo atualizado após sacar 200 reais do Pedro: "+c2.saldo);
-		
+
 		System.out.println("         ");
 		System.out.println("DESPÓSITO: ");
-		
-	    //método depositar
+
+		//método depositar
 		c1.despositar(1000);
 		System.out.println("Saldo atualizado após depositar 1000 reais do Robert: "+c1.saldo);
-		
+
 		c2.despositar(500);
 		System.out.println("Saldo atualizado após depositar 500 reais do Pedro: "+c2.saldo);
-	
+
 		System.out.println("         ");
-		System.out.println("Testando integração com o Github");
-		System.out.println("Testando integração com o Github 2");
-		
+		//System.out.println("Testando integração com o Github");
+		//System.out.println("Testando integração com o Github 2");
+
 	}
 }
